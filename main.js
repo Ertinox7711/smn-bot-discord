fe4c40620029ff0c24d5e5a9b482c51050823b64
Ertinox7711 <92356761+Ertinox7711@users.noreply.github.com>
2022-10-24 05:32:11 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,23 @@
 const Discord = require("discord.js");
 
-
-
 const client = new Discord.Client();
+
+const prefix = '-s';
 
 client.once('ready', () => {
     console.log('SMN est disponible')
 });
 
+client.on('message', message =>{
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/    );
+    const command = args.shift().tolowerCase();
+
+    if(command === 'ping'){
+        message.channel.send('pong!');
+    }
+})
 
 
 client.login(process.env.TOKEN);
