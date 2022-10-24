39ff4e2cf1ea769f7b16504ab26166001797dd11
Ertinox7711 <92356761+Ertinox7711@users.noreply.github.com>
2022-10-24 05:54:24 +0200

    Update main.js

--- main.js
+++ main.js
@@ -8,16 +8,12 @@ client.once('ready', () => {
     console.log('SMN est disponible')
 });
 
-client.on('message', message =>{
-    if(!message.content.startsWith(prefix) || message.author.bot) return;
 
-    const args = message.content.slice(prefix.length).split(/ +/    );
-    const command = args.shift().tolowerCase();
-
-    if(command === 'ping'){
-        message.channel.send('pong!');
+client.on('message', msg => {
+    if (msg.content === 'ping') {
+      msg.reply('Pong!');
     }
-})
+  });
 
 
 client.login(process.env.TOKEN);
